e702506d0b6066b94a2c5b5bcc6ca47632a04160
stavrosfa <67387767+stavrosfa@users.noreply.github.com>
2026-07-29 13:53:44 +0300

    Try to prevent godot from serializing texture images for theme override icons

--- C7/UIElements/Civ3HSlider.cs
+++ C7/UIElements/Civ3HSlider.cs
@@ -6,19 +6,18 @@ namespace C7.UIElements;
 [GlobalClass]
 [Tool]
 public partial class Civ3HSlider : HSlider {
-	[Export] public ImageTexture grabber { get; private set; }
-	public StyleBox grabberStyleBox { get; private set; }
-	[Export] public ImageTexture grabberHighlight { get; private set; }
-	public StyleBox grabberHighlightStyleBox { get; private set; }
-	[Export] public ImageTexture slider { get; private set; }
+	public ImageTexture grabber { get; private set; }
+	public ImageTexture grabberHighlight { get; private set; }
 	public StyleBox sliderStyleBox { get; private set; }
 
-	public Civ3HSlider AddGrabber(ImageTexture texture) => SetIcon("grabber", texture, t => grabber = t);
-	public Civ3HSlider AddGrabberStyleBox(StyleBox styleBox) => SetStyleBox("grabber", styleBox, s => grabberStyleBox = s);
-	public Civ3HSlider AddGrabberHighlight(ImageTexture texture) => SetIcon("grabber_highlight", texture, t => grabberHighlight = t);
-	public Civ3HSlider AddGrabberHighlightStyleBox(StyleBox styleBox) => SetStyleBox("grabber_highlight", styleBox, s => grabberHighlightStyleBox = s);
-	public Civ3HSlider AddSlider(ImageTexture texture) => SetIcon("slider", texture, t => slider = t);
-	public Civ3HSlider AddSliderStyleBox(StyleBox styleBox) => SetStyleBox("slider", styleBox, s => sliderStyleBox = s);
+	public Civ3HSlider AddGrabber(ImageTexture texture) =>
+		SetIcon("grabber", texture, t => grabber = t);
+
+	public Civ3HSlider AddGrabberHighlight(ImageTexture texture) =>
+		SetIcon("grabber_highlight", texture, t => grabberHighlight = t);
+
+	public Civ3HSlider AddSliderStyleBox(StyleBox styleBox) =>
+		SetStyleBox("slider", styleBox, s => sliderStyleBox = s);
 
 	private Civ3HSlider SetIcon(string name, ImageTexture texture, Action<ImageTexture> assign) {
 		assign(texture);
@@ -34,11 +33,34 @@ public partial class Civ3HSlider : HSlider {
 		return this;
 	}
 
-	public override void _ValidateProperty(Godot.Collections.Dictionary property) {
-		Util.ApplyNoSaveFlag(property, [
-				PropertyName.grabber,
-				PropertyName.grabberHighlight,
-				PropertyName.slider,
-			]);
+	// Unlike Civ3TextureButton or Civ3TextureRect where properties like Texture, TextureNormal, etc,
+	// are exposed in their parent classes, when we override a theme,
+	// godot seems to do some behind the scenes (pun not intended) magic
+	// so it's not enough to expose our custom texture fields,
+	// and then use _ValidateProperty and Util.ApplyNoSaveFlag
+	// to prevent godot from serializing the image textures.
+	// We do this "hacky" thing where before godot saves the scene in the editor,
+	// we remove all the theme overrides, godot saves the scenes, and re-applies them.
+	// It needs a bigger setup than I would prefer, but currently seems to be the most reliable way
+	// of preventing godot from serializing the textures.
+	// This is the post I found that helped with this (had to use some chatgpt help to translate it to c#):
+	// https://forum.godotengine.org/t/prevent-generated-resources-from-being-serialized-in-the-tscn/46562/3
+	public override void _Notification(int what) {
+		if (what == NotificationEditorPreSave) {
+			RemoveThemeIconOverride("grabber");
+			RemoveThemeIconOverride("grabber_highlight");
+			RemoveThemeStyleboxOverride("slider");
+		} else if (what == NotificationEditorPostSave) {
+			if (grabber != null) {
+				AddGrabber(grabber);
+			}
+			if (grabberHighlight != null) {
+				AddGrabberHighlight(grabberHighlight);
+			}
+
+			if (sliderStyleBox != null) {
+				AddSliderStyleBox(sliderStyleBox);
+			}
+		}
 	}
 }
